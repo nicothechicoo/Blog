4d8fbee50a0c8945a6ed2169e63b883d5bebd370
NicotheChico <nrhtome@gmail.com>
2023-09-25 15:36:46 -0700

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const emailButton=document.getElementById("emailbut")
+emailButton.addEventListener("click", (e) => {
+    copyCode()
+})
+const copyCode = () => { const tempTextArea = document.createElement('textarea');
+    tempTextArea.value = "nrhtome@gmail.com"
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempTextArea);
+}
