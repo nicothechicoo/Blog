514729fa9ab57ed66fd4c54cff828f8a3c01d34e
NicotheChico <nrhtome@gmail.com>
2023-11-05 15:06:10 +0900

    adding projects dropdown and first JS calculator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const emailButton=document.getElementById("emailbut")
 emailButton.addEventListener("click", (e) => {
     copyCode()
 })
+
 const copyCode = () => { const tempTextArea = document.createElement('textarea');
     tempTextArea.value = "nrhtome@gmail.com"
     document.body.appendChild(tempTextArea);
