f374927fadf98fac638e329ce6194e9de54c2fbf
NicotheChico <nrhtome@gmail.com>
2023-11-07 07:22:15 +0900

    added new pics and stuff

--- calculator.js
+++ calculator.js
@@ -1,39 +1,9 @@
 
-var num1 = 1;
-var num2 = 2;
-var num3 = 3;
-var num4 = 4;
-var num5 = 5;
-var num6 = 6;
-var num7 = 7;
-var num8 = 8;
-var num9 = 9;
-var num0 = 0;
-var add = "+";
-var sub = "-";
-var mul = "*";
-var div = "/";
-var calc = "calculate";
-
-function getElementById() {
-    var num1 = document.getElementById("1").value;
-    var num2 = document.getElementById("2").value;
-    var num3 = document.getElementById("3").value;
-    var num4 = document.getElementById("4").value;
-    var num5 = document.getElementById("5").value;
-    var num6 = document.getElementById("6").value;
-    var num7 = document.getElementById("7").value;
-    var num8 = document.getElementById("8").value;
-    var num9 = document.getElementById("9").value;
-    var num0 = document.getElementById("0").value;
-    var add = document.getElementById("add").value;
-    var sub = document.getElementById("sub").value;
-    var mul = document.getElementById("mul").value;
-    var div = document.getElementById("div").value;
-    var calc = document.getElementById("calc").value;
-    var answerbox = document.getElementById("answerbox").value;
-}
 
 
 
-
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-AC]');
+const submittButton = document.querySelector('[data-equals]');
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const emailButton=document.getElementById("emailbut")
+const emailButton = document.getElementById("emailbut")
 emailButton.addEventListener("click", (e) => {
     copyCode()
 })
 
-const copyCode = () => { const tempTextArea = document.createElement('textarea');
+const copyCode = () => {
+    const tempTextArea = document.createElement('textarea');
     tempTextArea.value = "nrhtome@gmail.com"
     document.body.appendChild(tempTextArea);
     tempTextArea.select();
