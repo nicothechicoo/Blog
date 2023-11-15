7f6492961e09b89447f8d37fdb4eef7c1e2758db
NicotheChico <nrhtome@gmail.com>
2023-11-15 11:14:28 -0800

    new navbar and js script

---
+++ setup.js
@@ -0,0 +1,21 @@
+const dropdownBg = document.querySelector(".dropdown-bg")
+
+Array.from(document.querySelectorAll(".nav-item"))
+.forEach(item => {
+    const lastChild = item.lastElementChild;
+    item.onmouseover = () => {
+        dropdownBg.style.opacity = "1";
+        dropdownBg.style.visibility = "visible";
+
+        dropdownBg.style.width = getComputedStyle(lastChild).width;
+        dropdownBg.style.height = getComputedStyle(lastChild).height;
+
+        dropdownBg.style.left = lastChild.offsetLeft + "px";
+        dropdownBg.style.top = lastChild.offsetTop + "px";
+    };
+
+    item.onmouseout = () => {
+        dropdownBg.style.opacity = "0";
+        dropdownBg.style.visibility = "hidden";
+    };
+})
